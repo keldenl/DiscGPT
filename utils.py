986368af3f7e266cbdacc6689c63821a312c28bf
keldenl <keldenlin@gmail.com>
2023-04-29 16:24:14 -0700

    remove some print logs

--- utils.py
+++ utils.py
@@ -1,8 +1,6 @@
 import re
 
 def create_id_name_hashmaps(messages):
-    print('MESSAGES')
-    print(messages)
     name_to_id = {f'@{message.name}': f'<@{message.id}>' for message in messages}
     id_to_name = {f'<@{message.id}>': f'@{message.name}' for message in messages}
     return name_to_id, id_to_name
